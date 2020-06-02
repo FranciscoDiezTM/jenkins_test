5c73875eb75a9cfc28debaffe5927b4e22f91751
Francisco Diez Garcia <francisco.diez@talentomobile.com>
2020-06-02 14:19:46 +0200

    Config updates

--- JenkinsTest/ViewController.swift
+++ JenkinsTest/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        #if PROD
+        print("We brew beer in the Production")
+        #else
+        print("We brew beer in the Debug")
+        #endif
     }
 
 
